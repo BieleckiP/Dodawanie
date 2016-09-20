51c7dc472211d2cd2f76f0a09a819ef7da99196d
Pawel B <bieleckip@gmail.com>
2016-09-20 23:19:19 +0200

    Revert "next step"
    This reverts commit 7f9decea3d7fffc51581d3997fce3f6e72e37ca4.

--- MainDodawanie.java
+++ MainDodawanie.java
@@ -2,7 +2,7 @@ import com.dodawanie.engine;
 
 class MainDodawanie{
     public static void main(String[] args){
-        System.out.println("Test of GIT");
+        System.out.println("Test");
         GetName gnWin = new GetName();
         gnWin.setVisible(true);
     }
